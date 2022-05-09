4680f569c777ce4748a81a19ab8a0832d04d7d39
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-09 19:02:13 +0900

    [#2] Chore: AppDelegate에도 SceneDelegate와 동일한 UIWindow 인스턴스 생성 로직 추가
    - iOS 13 이전 버전의 경우 SceneDelegate가 아닌 AppDelegate를 거치기 때문에 동일한 로직을 AppDelegate에도 작성

--- Starbucks/Starbucks/Supportings/AppDelegate.swift
+++ Starbucks/Starbucks/Supportings/AppDelegate.swift
@@ -3,6 +3,8 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
     }
@@ -11,7 +13,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        if #available(iOS 13.0, *) { return true }
+        
+        window = UIWindow()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
     }
 
 

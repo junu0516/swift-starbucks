b67b259fde068018fe7bb7527687b4acb42d742e
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-09 18:45:14 +0900

    [#2] Chore: 주석 제거, 스토리보드 제거, 디렉토리 정리
    - 주석 모두 제거
    - 스토리보드 사용하지 않을 것이기 때문에 완전히 삭제
    - AppDelegate, SceneDelegate Supportings 디렉토리 하위로 이동

--- Starbucks/Starbucks/ViewController.swift
+++ Starbucks/Starbucks/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Starbucks
-//
-//  Created by 이준우 on 2022/05/09.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 

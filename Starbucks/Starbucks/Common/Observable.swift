8a5a404aa2775c885b762d1fe2f78d9957a9690c
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-11 01:27:37 +0900

    [#5] Feat: 옵저버 패턴 사용을 위한 Observable 클래스 구현
    - 이번 프로젝트에서는 NotificationCenter 대신 Observable을 구현해서 사용

---
+++ Starbucks/Starbucks/Common/Observable.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+final class Observable<T> {
+    
+    private var listener: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ listener: @escaping (T) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}

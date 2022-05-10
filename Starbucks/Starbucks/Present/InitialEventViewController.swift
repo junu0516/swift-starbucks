ec4e0f2821c81d573cb9fb24a441acde2d9b2bf7
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-10 18:06:44 +0900

    [#2] Feat: 초기 이벤트 화면 구현 및 NetworkHandler을 사용하여 이벤트 이미지 요청하는 흐름 구현

---
+++ Starbucks/Starbucks/Present/InitialEventViewController.swift
@@ -0,0 +1,104 @@
+import Foundation
+import UIKit
+
+final class InitialEventViewController: UIViewController {
+    
+    private lazy var eventImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private lazy var buttonBackgroundView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var invalidationButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.borderWidth = 0.7
+        button.layer.borderColor = UIColor(named: "green_closeButton")?.cgColor
+        button.layer.cornerRadius = 15
+        button.setTitle("다시 보지 않기", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.addAction(UIAction(handler: { _ in
+            UserDefaults.standard.set(true, forKey: "eventInvalidated")
+            self.moveToMainTabbarView()
+        }), for: .touchDown)
+        return button
+    }()
+    
+    private lazy var closeButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("닫기", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = UIColor(named: "green_closeButton")
+        button.layer.cornerRadius = 15
+        button.addAction(UIAction(handler: { _ in
+            self.moveToMainTabbarView()
+        }), for: .touchDown)
+        return button
+    }()
+    
+    private let networkHandler: NetworkHandlable = NetworkHandler()
+    
+    override func loadView() {
+        super.loadView()
+        loadEventImage()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(eventImageView)
+        view.addSubview(buttonBackgroundView)
+        buttonBackgroundView.addSubview(invalidationButton)
+        buttonBackgroundView.addSubview(closeButton)
+        
+        view.backgroundColor = .white
+        setLayout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    private func setLayout() {
+        eventImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        eventImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        eventImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        eventImageView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.85).isActive = true
+        
+        buttonBackgroundView.topAnchor.constraint(equalTo: eventImageView.bottomAnchor).isActive = true
+        buttonBackgroundView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        buttonBackgroundView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        buttonBackgroundView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.15).isActive = true
+        
+        invalidationButton.widthAnchor.constraint(equalTo: buttonBackgroundView.widthAnchor, multiplier: 0.4).isActive = true
+        invalidationButton.leadingAnchor.constraint(equalTo: buttonBackgroundView.leadingAnchor, constant: 30).isActive = true
+        invalidationButton.centerYAnchor.constraint(equalTo: buttonBackgroundView.centerYAnchor).isActive = true
+        
+        closeButton.widthAnchor.constraint(equalTo: invalidationButton.widthAnchor).isActive = true
+        closeButton.trailingAnchor.constraint(equalTo: buttonBackgroundView.trailingAnchor, constant: -30).isActive = true
+        closeButton.centerYAnchor.constraint(equalTo: buttonBackgroundView.centerYAnchor).isActive = true
+    }
+    
+    private func moveToMainTabbarView() {
+        let tabbarView = MainTabBarController()
+        tabbarView.modalPresentationStyle = .fullScreen
+        self.present(tabbarView, animated: false)
+    }
+    
+    private func loadEventImage() {
+        networkHandler.request(url: .initialEventImage, method: .get, contentType: .image){ [self] result in
+            switch result {
+            case .success(let data):
+                self.eventImageView.image = UIImage(data: data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}

69ccb6238ebe5731833d58d8dc347b7236cc4d75
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-13 01:37:06 +0900

    [#4] Feat: HomeViewModel 구현 및 필요한 데이터 요청 흐름 구현

--- Starbucks/Starbucks/Common/EndPoint.swift
+++ Starbucks/Starbucks/Common/EndPoint.swift
@@ -3,6 +3,7 @@ import Foundation
 enum EndPoint {
     case initialEventImage
     case homeData
+    case mainEventImage
     
     var urlString: String {
         switch self {
@@ -10,6 +11,8 @@ enum EndPoint {
             return "https://s3.ap-northeast-2.amazonaws.com/lucas-image.codesquad.kr/1627033273796event-bg.png"
         case .homeData:
             return "https://api.codesquad.kr/starbuckst"
+        case .mainEventImage:
+            return "https://image.istarbucks.co.kr/upload/promotion/APP_THUM_20210719090612417.jpg"
         }
     }
 }
--- Starbucks/Starbucks/Present/Home/HomeViewModel.swift
+++ Starbucks/Starbucks/Present/Home/HomeViewModel.swift
@@ -1,5 +1,45 @@
 import Foundation
+import OSLog
 
 final class HomeViewModel {
+    
+    private (set) var eventImageData = Observable<Data>(Data())
+    private (set) var displayName = Observable<String>("USER NAME")
+    private (set) var personalRecommendations = Observable<[Recommendation]>([])
+    private (set) var timeRecommendations = Observable<[Recommendation]>([])
+
+    private var networkHandler: NetworkHandlable?
+    private var jsonHandler: JSONHandlable = JSONHandler()
+    private let logger = Logger()
+    
+    init(networkHandler: NetworkHandlable) {
+        self.networkHandler = networkHandler
+        loadMainImageData()
+        loadUserData()
+    }
+    
+    func loadUserData() {
+        sendApiRequest(url: .homeData, method: .get, contentType: .json) { [weak self] data in
+            guard let response = self?.jsonHandler.convertJSONToObject(from: data, to: HomeDataResponse.self) else { return }
+            print(response)
+            self?.displayName.value = response.displayName
+        }
+    }
+    
+    func loadMainImageData() {
+        sendApiRequest(url: .mainEventImage, method: .get, contentType: .image) { [weak self] data in
+            self?.eventImageData.value = data
+        }
+    }
+    private func sendApiRequest(url: EndPoint, method: HttpMethod, contentType: ContentType, successHandler: @escaping (Data) -> Void) {
+        networkHandler?.request(url: url, method: method, contentType: contentType) { [weak self] result in
+            switch result {
+            case .success(let data):
+                successHandler(data)
+            case .failure(let error):
+                self?.logger.error("\(error.localizedDescription)")
+            }
+        }
+    }
     
 }

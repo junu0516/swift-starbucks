debcf42d9d94f6601e85abeb2c63782e495572d8
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-15 10:31:02 +0900

    [#7] Feat: RecommendationViewController에 뷰 관련 레이아웃, 데이터바인딩 설정 로직 추가

--- Starbucks/Starbucks/Present/Recommendation/RecommendationViewController.swift
+++ Starbucks/Starbucks/Present/Recommendation/RecommendationViewController.swift
@@ -18,6 +18,38 @@ final class RecommendationViewController: UIViewController {
         self.init()
         self.recommendationViewModel = recommendationViewModel
         self.category = category
+        //addViews()
+        //setLayout()
+        bind()
     }
     
+    private func addViews() {
+        view.addSubview(collectionView)
+    }
+    
+    private func setLayout() {
+        collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+    
+    private func bind() {
+        recommendationViewModel?.recommendations.bind { recommendation in
+            if recommendation.count <= 0 { return }
+            //동기적으로 이미지 데이터 요청해서 뷰모델의 이미지 데이터 배열 업데이트
+        }
+    }
+}
+
+extension RecommendationViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let recommendation = recommendationViewModel?.recommendations.value as? [Data] else { return 0 }
+        return recommendation.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        return UICollectionViewCell()
+    }
 }

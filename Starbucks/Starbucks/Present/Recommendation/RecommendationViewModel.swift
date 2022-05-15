075d5250244e3065441f5a1e65edf1ea0144af03
Joonwoo Lee <68586291+junu0516@users.noreply.github.com>
2022-05-16 00:20:39 +0900

    [#8] Feat: RecommendationViewModel 내부에서 이미지 바이너리 데이터 요청하는 흐름 구현

--- Starbucks/Starbucks/Present/Recommendation/RecommendationViewModel.swift
+++ Starbucks/Starbucks/Present/Recommendation/RecommendationViewModel.swift
@@ -1,11 +1,46 @@
 import Foundation
+import OSLog
 
 final class RecommendationViewModel {
     
     private (set) var recommendations = Observable<[Data]>([])
+    private (set) var productImageList = Observable<[Data]>([])
+    
+    private let logger = Logger()
+    private let jsonHandler: JSONHandlable = JSONHandler()
     private var networkHandler: NetworkHandlable?
+    private var semaphore = DispatchSemaphore(value: 0)
+
 
     init(networkHandler: NetworkHandlable) {
         self.networkHandler = networkHandler
+    }
+    
+    func loadProductImageData() {
+        var imageDataList: [Data] = []
+        let requestDataList = recommendations.value
+        requestDataList.forEach { [weak self] in
+            guard let requestDataList = self?.jsonHandler.convertJSONToObject(from: $0, to: ImageFileRequestList.self) else { return }
+            if requestDataList.file.count <= 0 { return }
+            let requestData = requestDataList.file[0]
+            let url = EndPoint.productImageData(filePath: requestData.filePath, fileUrl: requestData.fileUrl)
+            self?.sendApiRequest(url: url, method: .get, contentType: .image, body: nil) { data in
+                imageDataList.append(data)
+                self?.semaphore.signal()
+            }
+            semaphore.wait()
+        }
+        productImageList.value = imageDataList
+    }
+    
+    private func sendApiRequest(url: EndPoint, method: HttpMethod, contentType: ContentType, body: Data?, successHandler: @escaping (Data) -> Void) {
+        networkHandler?.request(url: url, method: method, contentType: contentType, body: body) { [weak self] result in
+            switch result {
+            case .success(let data):
+                successHandler(data)
+            case .failure(let error):
+                self?.logger.error("\(error.localizedDescription)")
+            }
+        }
     }
 }
